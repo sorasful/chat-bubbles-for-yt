faeaa09c1636e267be469538867ac1622c6a9920
Téva KRIEF <32820423+sorasful@users.noreply.github.com>
2025-07-09 15:41:49 +0200

    Modifier le comportement de fermeture du menu settings

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -49,7 +49,6 @@ const Settings = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            onClick={toggleSettings}
           />
           
           {/* Sidebar */}
